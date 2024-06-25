f53186cc9ea4126e76afc39d4cc7251cbf5a11a7
Adam Evans <adam3van5@gmail.com>
2024-06-25 19:26:46 +0900

    finish log component

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,3 +1,11 @@
-export default function Log() {
-  return <ul id="log"></ul>;
+export default function Log({ turns }) {
+  return (
+    <ul id="log">
+      {turns.map((turn) => (
+        <li key={`${turn.square.row}${turn.square.col}`}>
+          Player {turn.player} selected {turn.square.row}, {turn.square.col}
+        </li>
+      ))}
+    </ul>
+  );
 }
