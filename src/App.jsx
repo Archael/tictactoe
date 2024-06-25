534eb2cecc0f3f9d3a99456db0a8ed0bbe89e9ef
Adam Evans <adam3van5@gmail.com>
2024-06-25 17:53:26 +0900

    complete logic, add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
+import { useState } from 'react';
 import Player from './components/Player';
 import GameBoard from './components/Gameboard';
-
+import Log from './components/Log';
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActivePlayer((player) => (player === 'X' ? '0' : 'X'));
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="0" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === 'X'}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="0"
+            isActive={activePlayer === '0'}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
-      LOG
+      <Log />
     </main>
   );
 }
